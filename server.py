5c89138d57623940e24e79ad588ba3a1303d1867
unknown <joelsoro273@gmail.com>
2025-07-03 13:46:46 +0200

    Initial render deploy

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/waifu',  methods=['POST'])
+def waifu():
+  data = request.json
+  message = data.get("message","")
+  print(f"Message reçu : {message}")
+
+  # Réponse simulée avec une fausse URL de video
+  return jsonify({"video_url":
+"http://example.com/video.mp4"})
+
+if __name__ =='__main__':
+  app.run(host="0.0.0.0", port=5000)
